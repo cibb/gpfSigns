2c40d4eb53abc702e390ad42f5b82dcc5d98b313
Dennis Tobar <dennis.tobar@gmail.com>
2016-06-08 15:39:45 -0400

    Cambios en mensajes y lógica JS
    * Ajusté la web de Adrià en los agradecimientos .tk => .com
    * Cambié el cálculo del tamaño por .fadeToggle() de jQuery
    * CSS para el botón de añadir nueva firma
    * Espaciados
    * Cambio en el selector del espacio de firma

--- js/config.js
+++ js/config.js
@@ -18,80 +18,78 @@ function localizeHtmlPage() {
 }
 localizeHtmlPage();
 
-chrome.storage.sync.get(null, function(items) {
-  var mySigns = (items.signs ? JSON.parse(items.signs) : []);
+chrome.storage.sync.get(null, function (items) {
+    var mySigns = (items.signs ? JSON.parse(items.signs) : []);
 
-  $(document).ready(function () {
-      $("#input").cleditor();
-  });
+    $(document).ready(function () {
+        $("#input").cleditor();
+    });
 
-  $("#createNewBtn").click(function () {
-      if (document.getElementById('newSignBox').style.height == '0px') {
-          document.getElementById('newSignBox').style.height = '380px';
-      } else {
-          document.getElementById('newSignBox').style.height = '0px';
-      }
-  });
+    $("#createNewBtn").click(evt => {
+        $('#newSignBox').fadeToggle();
+    });
 
-  $("#addSigns").click(function () {
-      var myBol = true
+    $("#addSigns").click(function () {
 
-      if ($("#before").children(":selected").attr("id") == "true")
-          myBol = false;
+        var myBol = !($('#before option:selected').attr('id') == "true")
 
-      if ($("#inpName").val().length < 2 || $("#inpName").val().length > 30) {
-          alert(chrome.i18n.getMessage("errorNamelenght"));
-          return;
-      }
+        if ($("#inpName").val().length < 2 || $("#inpName").val().length > 30) {
+            alert(chrome.i18n.getMessage("errorNamelenght"));
+            return;
+        }
 
-      var myAdded = {
-          'name': $("#inpName").val(),
-          'post': myBol,
-          'used': 0,
-          'message': $("#input").val()
-      }
+        var myAdded = {
+            'name': $("#inpName").val(),
+            'post': myBol,
+            'used': 0,
+            'message': $("#input").val()
+        }
 
-      mySigns.push(myAdded);
+        mySigns.push(myAdded);
 
-      chrome.storage.sync.set({"signs":JSON.stringify(mySigns)});
+        chrome.storage.sync.set({
+            "signs": JSON.stringify(mySigns)
+        });
 
-      myJ = [
-              $("#inpName").val(),
-              0,
-              '<button id="delete" data-id="' + (mySigns.length - 1) + '"> Borrar </button>'
+        myJ = [
+            $("#inpName").val(),
+            0,
+            '<button id="delete" data-id="' + (mySigns.length - 1) + '"> Borrar </button>'
+        ];
+        table.row.add(myJ).draw();
+
+        $("#inpName").val("");
+        $("#input").cleditor()[0].clear()
+
+        alert(chrome.i18n.getMessage("savedSignDone"));
+    });
+
+    $(document).ready(function () {
+        table = $('#dtable').DataTable({
+            "info": false
+        });
+
+        for (var roow in mySigns) {
+            myJ = [
+                mySigns[roow].name,
+                mySigns[roow].used,
+                '<button id="delete" data-id="' + (mySigns.length - 1) + '"> Borrar </button>'
             ];
-      table.row.add(myJ).draw();
+            table.row.add(myJ).draw();
+        };
+    });
 
-      $("#inpName").val("");
-      $("#input").cleditor()[0].clear()
+    $("#dtable").on("click", "#delete", (function () {
+        var newJs = [];
+        for (var roow in mySigns) {
+            if (roow != $(this).attr("data-id"))
+                newJs.push(mySigns[roow]);
+        };
+        table.row($(this).parents('tr')).remove().draw();
+        mySigns = newJs;
 
-      alert(chrome.i18n.getMessage("savedSignDone"));
-  });
-
-  $(document).ready(function () {
-      table = $('#dtable').DataTable({
-          "info": false
-      });
-
-      for (var roow in mySigns) {
-          myJ     = [
-                      mySigns[roow].name,
-                      mySigns[roow].used,
-                      '<button id="delete" data-id="' + (mySigns.length - 1) + '"> Borrar </button>'
-                    ];
-          table.row.add(myJ).draw();
-      };
-  });
-
-  $("#dtable").on("click", "#delete", (function () {
-      var newJs = [];
-      for (var roow in mySigns) {
-          if (roow != $(this).attr("data-id"))
-              newJs.push(mySigns[roow]);
-      };
-      table.row($(this).parents('tr')).remove().draw();
-      mySigns = newJs;
-
-      chrome.storage.sync.set({"signs":JSON.stringify(mySigns)});
-  }));
+        chrome.storage.sync.set({
+            "signs": JSON.stringify(mySigns)
+        });
+    }));
 });

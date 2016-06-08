1c239fb7c2b9a3717e6c8651d51fcd6387f0a7c7
Dennis Tobar <dennis.tobar@gmail.com>
2016-06-08 16:15:12 -0400

    Separando localize del código de la extensión

--- js/config.js
+++ js/config.js
@@ -1,22 +1,6 @@
+/**globals document */
+
 var table;
-
-function localizeHtmlPage() {
-    //Localize by replacing __MSG_***__ meta tags
-    var objects = document.getElementsByTagName('html');
-    for (var j = 0; j < objects.length; j++) {
-        var obj = objects[j];
-
-        var valStrH = obj.innerHTML.toString();
-        var valNewH = valStrH.replace(/__MSG_(\w+)__/g, function (match, v1) {
-            return v1 ? chrome.i18n.getMessage(v1) : "";
-        });
-
-        if (valNewH != valStrH) {
-            obj.innerHTML = valNewH;
-        }
-    }
-}
-localizeHtmlPage();
 
 chrome.storage.sync.get(null, function (items) {
     var mySigns = (items.signs ? JSON.parse(items.signs) : []);
